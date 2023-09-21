8ef40784a0666df6ac11596d1e5533fdc0f04816
NaeemaBsharat <naeemabsharat1@gmail.com>
2023-09-21 17:59:43 +0300

    Update javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,50 @@
-var selectedRow= null;
+var selectedRow = null;
 
 
 // showing alerts--------------------------------//
 
-function showAlert(message, className){
+function showAlert(message, className) {
     const div = document.createElement("div");
     div.className = `alert alert-${className}`;
 
     div.appendChild(document.createTextNode(message));
     const container = document.querySelector(".container");
-    const main= document.querySelector(".main");
+    const main = document.querySelector(".main");
     container.insertBefore(div, main);
 
 
-    setTimeout( ()=> document.querySelector(".alert").remove(), 3000);
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
 
 }
 
 // clear all data code------------------------------//
 
-function clearFields(){
-    document.querySelector("Task").value="";
-    document.querySelector("Day").value="";
-    document.querySelector("Time").value="";
+function clearFields() {
+    document.querySelector("Task").value = "";
+    document.querySelector("Day").value = "";
+    document.querySelector("Time").value = "";
 
 }
 
 // ADD data code ---------------------------------//
 
-document.querySelector("#Task-form").addEventListener("submit", (e) =>{
+document.querySelector("#Task-form").addEventListener("submit", (e) => {
     e.preventDefault();
 
 
-const Task = document.querySelector("#Task").value;
-const Day = document.querySelector("#Day").value;
-const Time = document.querySelector("#Time").value;
+    const Task = document.querySelector("#Task").value;
+    const Day = document.querySelector("#Day").value;
+    const Time = document.querySelector("#Time").value;
 
-if(Task =="" || Day=="" || Time=="") {
-    showAlert("please fill all fields" ,"danger");
-}
-else{
-if(selectedRow == null){
-    const list = document.querySelector("#Task-list");
-    const row = document.createElement("tr");
+    if (Task == "" || Day == "" || Time == "") {
+        showAlert("please fill all fields", "danger");
+    }
+    else {
+        if (selectedRow == null) {
+            const list = document.querySelector("#Task-list");
+            const row = document.createElement("tr");
 
-    row.innerHTML=`
+            row.innerHTML = `
     <td>${Task}</td>
     <td>${Day}</td>
     <td>${Time}</td>
@@ -54,47 +54,46 @@ if(selectedRow == null){
 
     
     `;
-    list.appendChild(row);
-    selectedRow = null;
-    showAlert("task Added", "success");
-  }
-  else{
-    selectedRow.children[0].textContent = Task;
-    selectedRow.children[1].textContent = Day;
-    selectedRow.children[2].textContent = Time;
-    selectedRow = null;
-    showAlert("Task Edited","succ");
+            list.appendChild(row);
+            selectedRow = null;
+            showAlert("task Added", "success");
+        }
+        else {
+            selectedRow.children[0].textContent = Task;
+            selectedRow.children[1].textContent = Day;
+            selectedRow.children[2].textContent = Time;
+            selectedRow = null;
+            showAlert("Task Edited", "succ");
 
-  }
-  clearFields();
- }
+        }
+        clearFields();
+    }
 });
 
 // Edit Data code---------------------//
 
-document.querySelector("#Task-list").addEventListener("click", (e) =>{
+document.querySelector("#Task-list").addEventListener("click", (e) => {
     target = e.target;
-    if(target.classList.contains("edit")){
+    if (target.classList.contains("edit")) {
         selectedRow = target.parentElement.parentElement;
-        document.querySelector("#Task").value =selectedRow.children[0].textContent;
-        document.querySelector("#Day").value =selectedRow.children[1].textContent;
-        document.querySelector("#Time").value =selectedRow.children[2].textContent;
+        document.querySelector("#Task").value = selectedRow.children[0].textContent;
+        document.querySelector("#Day").value = selectedRow.children[1].textContent;
+        document.querySelector("#Time").value = selectedRow.children[2].textContent;
     }
 
 });
 
 // Delete data code------------------------//
-document.querySelector("#Task-list").addEventListener("click", (e) =>{
+document.querySelector("#Task-list").addEventListener("click", (e) => {
     target = e.target;
-    if(target.classList.contains("delete")){
+    if (target.classList.contains("delete")) {
         target.parentElement.parentElement.remove();
-        showAlert("Task Deleted" ,"danger");
+        showAlert("Task Deleted", "danger");
     }
 });
 
-
 // Local code---------------------------
-// Check if local storage is supported
+
 function isLocalStorageSupported() {
     try {
         localStorage.setItem('test', 'test');
@@ -105,7 +104,6 @@ function isLocalStorageSupported() {
     }
 }
 
-// Check if local storage is supported and show/hide the project accordingly
 if (isLocalStorageSupported()) {
     document.getElementById('project').style.display = 'block';
 } else {
